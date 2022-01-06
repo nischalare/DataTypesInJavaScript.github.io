c300f7655c22b8deebe8acd979754696272bab4f
Nischal <nischal.aremanda1432@gmail.com>
2022-01-06 09:45:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+x = prompt("Please Enter Your Name:")
+alert("Welcome " + x + " For Learning JavaScript DataTypes")
+
+function myFunction() {
+    document.getElementById(
+      "demo").innerHTML = "JavaScript variables can hold different data types: numbers, strings, objects and more";
+}
+
+
+
+
+
